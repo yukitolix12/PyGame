b3844a7a4910c416b1e9361bdfd12f00c22a5d56
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-01 17:22:29 +0900

    pygameの画面表示

--- sample.py
+++ sample.py
@@ -2,4 +2,10 @@ import pygame
 
 pygame.init()
 
-pygame.display.set_model((800, 600))
+screen = pygame.display.set_mode((800, 600))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False

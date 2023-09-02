00a3fda7a4100d9cd18bc8bb31d794d72049d1e9
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-02 10:23:35 +0900

    playerを表示

--- sample.py
+++ sample.py
@@ -7,21 +7,26 @@ screen = pygame.display.set_mode((800, 600))
 #screen.fill((150,150,150))
 pygame.display.set_caption('invaders Game')
 
-#Player
+# Player
 playerImg = pygame.image.load('player.png')
 playerX,playerY = 370, 480
 playerX_change = 0
 
-mixer.Sound('laser.wav').play()
+# mixer.Sound('laser.wav').play()
+
+def player(x, y):
+    screen.blit(playerImg, (x,y))
 
 running = True
 while running:
-    screen.blit(img, (X,Y))
-    font = pygame.font.SysFont(None,80)
-    message = font.render('Hello world',False,(255,255,255))
-    screen.blit(message, (20,50))
+    
+    # font = pygame.font.SysFont(None,80)
+    # message = font.render('Hello world',False,(255,255,255))
+    # screen.blit(message, (20,50))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    player(playerX, playerY)
+    
     pygame.display.update()

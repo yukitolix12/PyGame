6658b8ce73cf68940eba0c4265bd397dbaf74765
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-02 10:07:10 +0900

    画像を表示

--- sample.py
+++ sample.py
@@ -6,8 +6,13 @@ screen = pygame.display.set_mode((800, 600))
 screen.fill((150,150,150))
 pygame.display.set_caption('invaders Game')
 
+img = pygame.image.load('player.png')
+X = 370
+Y = 480
+
 running = True
 while running:
+    screen.blit(img, (X,Y))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False

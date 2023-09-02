912afb21e455c3287811df96a7beed78f44f26a8
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-02 10:02:27 +0900

    背景色を設定

--- sample.py
+++ sample.py
@@ -3,6 +3,7 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
+screen.fill((150,150,150))
 pygame.display.set_caption('invaders Game')
 
 running = True
@@ -10,3 +11,5 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    pygame.display.update()

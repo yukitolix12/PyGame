e69b2d1a7a4e4a0786d8ec0a2ff56512f7dc1c50
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-09-02 10:18:39 +0900

    player配置

--- sample.py
+++ sample.py
@@ -4,12 +4,13 @@ from pygame import mixer
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
-screen.fill((150,150,150))
+#screen.fill((150,150,150))
 pygame.display.set_caption('invaders Game')
 
-img = pygame.image.load('player.png')
-X = 370
-Y = 480
+#Player
+playerImg = pygame.image.load('player.png')
+playerX,playerY = 370, 480
+playerX_change = 0
 
 mixer.Sound('laser.wav').play()
 
